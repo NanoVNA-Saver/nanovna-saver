6df2d76eea48fe41652fbd1e8c08939c067f34e6
Rune B. Broberg <mihtjel@gmail.com>
2019-08-28 10:29:04 +0200

    Ugh, more Copyright

--- nanovna-saver.py
+++ nanovna-saver.py
@@ -1,5 +1,4 @@
 #  Copyright 2019 Rune B. Broberg
-#  This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
 from time import sleep
 from PyQt5 import QtWidgets, QtCore, QtGui
