9da68d69fc16a0d754aa8980b7c09c7bd6818955
Rune B. Broberg <mihtjel@gmail.com>
2019-08-29 15:10:35 +0200

    GNU General Public License v3

--- Marker.py
+++ Marker.py
@@ -1,4 +1,18 @@
-#  Copyright (c) $year Rune B. Broberg
+#  NanoVNASaver - a python program to view and export Touchstone data from a NanoVNA
+#  Copyright (C) 2019.  Rune B. Broberg
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import collections
 from typing import List
 
