d733a384b13b0efb069d1cef0fe74fe818c57652
Rune Broberg <mihtjel@gmail.com>
2019-09-30 13:46:58 +0200

    - 0.1.0alpha

--- NanoVNASaver/about.py
+++ NanoVNASaver/about.py
@@ -14,5 +14,5 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-version = '0.0.12'
+version = '0.1.0alpha'
 
