cec746cb6486580398db4794395a690479bc1bc8
Holger Mueller <zarath@gmx.de>
2019-11-26 10:15:46 +0100

    Import path fix for unittests

--- test/test_sitools.py
+++ test/test_sitools.py
@@ -99,6 +99,3 @@ class TestTSIToolsValue(unittest.TestCase):
         self.assertEqual(str(v.parse("1e-2")), "10m")
         self.assertEqual(str(v.parse("1e-3")), "1m")
 
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
