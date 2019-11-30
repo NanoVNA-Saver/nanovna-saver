1e7faf1b1ccd8c691a65bb46b46552208c061d59
Holger Mueller <zarath@gmx.de>
2019-11-30 22:09:24 +0100

    use annotations to express type hint returning self

--- NanoVNASaver/SITools.py
+++ NanoVNASaver/SITools.py
@@ -14,6 +14,7 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from __future__ import annotations
 import math
 import decimal
 from typing import NamedTuple, Union
