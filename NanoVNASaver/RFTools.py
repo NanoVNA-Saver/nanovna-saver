6d74ae52a356506c603becadc9cbcdae708e57f2
Holger Müller <zarath@gmx.de>
2020-07-26 11:47:12 +0200

    Phase of complex cannot be > 2 * pi

--- NanoVNASaver/RFTools.py
+++ NanoVNASaver/RFTools.py
@@ -90,11 +90,6 @@ def groupDelay(data: List[Datapoint], index: int) -> float:
     delta_freq = data[idx1].freq - data[idx0].freq
     if delta_freq == 0:
         return 0
-    if abs(delta_angle) > math.tau:
-        if delta_angle > 0:
-            delta_angle = delta_angle % math.tau
-        else:
-            delta_angle = -1 * (delta_angle % math.tau)
     val = -delta_angle / math.tau / delta_freq
     return val
 
