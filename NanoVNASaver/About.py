cd1f32e9dc8f3ac064028f1ffc089e3da77d7f70
Holger Müller <zarath@gmx.de>
2020-10-01 13:42:52 +0200

    Version suffix for dev branch

--- NanoVNASaver/About.py
+++ NanoVNASaver/About.py
@@ -17,7 +17,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-VERSION = "0.3.8-pre01"
+VERSION = "0.3.8-dev"
 VERSION_URL = (
     "https://raw.githubusercontent.com/"
     "NanoVNA-Saver/nanovna-saver/master/NanoVNASaver/About.py")
