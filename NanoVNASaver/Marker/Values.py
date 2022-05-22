8fe83b05dbb8a957aa16d694ed40f6d49a075ead
DEVIGE <fabrice.devige@invoxia.com>
2022-05-22 15:39:23 +0200

    Cosmetic

--- NanoVNASaver/Marker/Values.py
+++ NanoVNASaver/Marker/Values.py
@@ -59,7 +59,7 @@ TYPES = (
     Label("s21realimagshunt", "S21 R+jX shunt", "S21 Z Real+Imag shunt", False),
     Label("s21realimagseries", "S21 R+jX series", "S21 Z Real+Imag series", False),
 
-    Label("imp_matching", "Match", "Components for 50 ohms impedance matching", False),
+    Label("imp_matching", "Matching", "Components for 50 ohms impedance matching", False),
 )
 
 
