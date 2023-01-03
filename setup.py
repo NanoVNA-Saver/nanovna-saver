d654ea0441939e4e1c599d1333b587a185394fbe
Holger Müller <zarath@gmx.de>
2023-01-03 16:38:43 +0100

    Removed Changelog.md from setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
   data_files=[
-    ( "share/doc/nanovnasaver/", [ "CHANGELOG.md", "LICENSE", "README.md" ] ),
+    ( "share/doc/nanovnasaver/", [ "LICENSE", "README.md" ] ),
     ( "share/applications/", [ "NanoVNASaver.desktop" ] ),
     ( "share/icons/hicolor/48x48/apps/", [ "NanoVNASaver_48x48.png" ] ),
   ]
