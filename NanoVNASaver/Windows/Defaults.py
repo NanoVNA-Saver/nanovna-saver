185a64b5ae51b4b21cb01ad4442fad8978993f8a
Holger Müller <zarath@gmx.de>
2023-02-28 21:18:00 +0100

    removed unused imports

--- NanoVNASaver/Windows/Defaults.py
+++ NanoVNASaver/Windows/Defaults.py
@@ -17,9 +17,8 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import logging
-from typing import Tuple
 
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets
 
 logger = logging.getLogger(__name__)
 
