8d70d882cdeffbe5cd36361cce80fd915aa8e0a7
Nicolas Boulenguez <nicolas@debian.org>
2023-03-22 08:02:07 +0100

    Style: simplify extraction of version from metadata

--- src/NanoVNASaver/__init__.py
+++ src/NanoVNASaver/__init__.py
@@ -1,13 +1,7 @@
-from importlib.metadata import (
-    PackageNotFoundError,
-    version,
-)
+import importlib.metadata
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = "nanovna-saver"
-    __version__ = version(dist_name)
-except PackageNotFoundError:  # pragma: no cover
+    __version__ = importlib.metadata.version(distribution_name="nanovna-saver")
+except importlib.metadata.PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
-finally:
-    del version, PackageNotFoundError
