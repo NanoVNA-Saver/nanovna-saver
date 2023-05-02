NanoVNA-Saver/nanovna-saver

commit b0110002ec2b001a432b4b44fd94839dfdb26f1b

moved to pyscaffold directory structure
and fixed resonances analysys crash #608

nanovna-saver.py
@@ -24,7 +24,13 @@ with suppress(ImportError):
     # pyright: reportMissingImports=false
     import pkg_resources.py2_warn
 
-from NanoVNASaver.__main__ import main
+try:
+    from NanoVNASaver.__main__ import main
+except ModuleNotFoundError:
+    import sys
+    sys.path.append('src')
+    from NanoVNASaver.__main__ import main
+
 
 if __name__ == '__main__':
     main()

commit c804388fa3c5c0728d3457c2e3c75491cc3df76a

nanovna-saver.py: fix execution from outside source directory
nanovna-saver.py can be called from outside the source directory. The
module import path needs to be resolved relative to the source
directory, not relative to the current working directory of the
process.

Fixes: b0110002 ("moved to pyscaffold directory structure")

nanovna-saver.py
@@ -17,6 +17,7 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from contextlib import suppress
+import os
 
 # noinspection PyUnresolvedReferences
 with suppress(ImportError):
@@ -29,7 +30,7 @@ try:
 except ModuleNotFoundError:
     import sys
 
-    sys.path.append("src")
+    sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
     from NanoVNASaver.__main__ import main
 
 