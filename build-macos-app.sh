aab2a15f69fd2b7bc3d5f2663a18f5878af8cb2e
Crispin Tschirky <ct@fhr.ch>
2023-05-05 10:40:54 +0200

    Build of MacOS app bundle NanoVNASaver.app  (#634)
    * add icon for MacOS based on icon_48x48.png
    * add macos build script to build NanoVNASaver.app for current architecture
    * add releas app action to build: NanoVNASaver.app-x86_64.tar.gz action

---
+++ build-macos-app.sh
@@ -0,0 +1,21 @@
+# Builds a NanoVNASaver.app on MacOS
+# ensure you have pyqt >=6.4 installed (brew install pyqt)
+#
+export VENV_DIR=macbuildenv
+
+# setup build venv
+python3 -m venv ${VENV_DIR}
+. ./${VENV_DIR}/bin/activate
+
+# install required dependencies (pyqt libs must be installed on the system)
+python3 -m pip install pip==23.0.1 setuptools==67.6.0
+pip install -r requirements.txt
+pip install PyInstaller==5.9.0
+
+python3 setup.py -V
+
+pyinstaller --onedir -p src -n NanoVNASaver nanovna-saver.py --window --clean  -y -i icon_48x48.icns
+tar -C dist -zcf ./dist/NanoVNASaver.app-`uname -m`.tar.gz  NanoVNASaver.app
+
+deactivate
+rm -rf ${VENV_DIR}
