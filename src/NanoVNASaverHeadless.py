ee2d45363e67da45a15e83b7a6cfc03dd39bc725
tbergkvist <bergkvist.teo@protonmail.com>
2024-04-05 15:56:02 +0200

    Update NanoVNASaverHeadless with a class structure.

--- src/NanoVNASaverHeadless.py
+++ src/NanoVNASaverHeadless.py
@@ -9,12 +9,36 @@ vna = hw.get_VNA(iface)
 
 #print(hw.get_portinfos())
 #print(hw.detect_version(iface))
-print("VNA is connected: ", vna.connected())
-vna.reconnect()
 
-print(vna.readFirmware())
-print(vna.read_features())
 print(vna.setSweep(1000, 2000))
 data = vna.readValues("data 0")
 
 vna.disconnect()
+
+class NanoVNASaverHeadless:
+    def __init__(self, vna_index=0, verbose=False):
+        self.verbose = verbose
+        self.iface = hw.get_interfaces()[vna_index]
+        self.vna = hw.get_VNA(iface)
+        if self.verbose:
+            print("VNA is connected: ", self.vna.connected())
+            print("Firmware: ", self.vna.readFirmware())
+            print("Features: ", self.vna.read_features())
+
+    def calibrate(self):
+        pass
+
+    def set_sweep(self):
+        pass
+
+    def stream_data(self):
+        pass
+
+    def kill(self):
+        self.vna.disconnect()
+        if self.vna.connected():
+            raise Exception("The VNA was not successfully disconnected.")
+        else:
+            if self.verbose:
+                print("Disconnected VNA.")
+            return
