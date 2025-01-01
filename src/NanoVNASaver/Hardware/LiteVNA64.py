b82b3b6eaa357dda8141329b8857454e53684684
Maxim Medvedev <redrathnure@gmail.com>
2025-01-01 16:33:07 +0100

    feat: add LiteVNA64 detection
    Based on https://github.com/NanoVNA-Saver/nanovna-saver/issues/534#issuecomment-1837198040 data

---
+++ src/NanoVNASaver/Hardware/LiteVNA64.py
@@ -0,0 +1,83 @@
+from .NanoVNA_V2 import (
+    NanoVNA_V2,
+    _CMD_READ,
+    _ADDR_FW_MAJOR,
+    _ADDR_FW_MINOR,
+    _ADDR_DEVICE_VARIANT,
+    _ADDR_HARDWARE_REVISION,
+)
+import logging
+import platform
+from struct import pack, unpack_from
+from time import sleep
+
+from NanoVNASaver.Hardware.Serial import Interface
+from NanoVNASaver.Hardware.VNA import VNA
+from NanoVNASaver.Version import _Version as Version, Version as parse_version
+from serial import Serial
+
+if platform.system() != "Windows":
+    import tty
+
+logger = logging.getLogger(__name__)
+
+EXPECTED_HW_VERSION = Version(2, 2, 0, "")
+EXPECTED_FW_VERSION = Version(2, 2, 0, "")
+
+
+class LiteVNA64(NanoVNA_V2):
+    name = "LiteVNA-64"
+    valid_datapoints = (101, 11, 51, 201, 301, 501, 1023, 2047, 4095)
+    screenwidth = 480
+    screenheight = 320
+    sweep_points_max = 65535
+
+    def __init__(self, iface: Interface):
+        super().__init__(iface)
+
+    @staticmethod
+    def _read_major_minor_version(
+        cmd_major_version: int, cmd_minor_version: int, serial: Serial
+    ) -> Version:
+        cmd = pack(
+            "<BBBB", _CMD_READ, cmd_major_version, _CMD_READ, cmd_minor_version
+        )
+
+        serial.write(cmd)
+        # sleep(WRITE_SLEEP)
+        sleep(2.0)  # could fix bug #585 but shoud be done
+        # in a more predictive way
+        resp = serial.read(2)
+
+        if len(resp) != 2:  # noqa: PLR2004
+            logger.error("Timeout reading version registers. Got: %s", resp)
+            raise IOError("Timeout reading version registers")
+        return parse_version(f"{resp[0]}.{resp[1]}")
+
+    @staticmethod
+    def read_fw_version(serial: Serial) -> Version:
+        result = LiteVNA64._read_major_minor_version(
+            _ADDR_FW_MAJOR, _ADDR_FW_MINOR, serial
+        )
+        logger.debug("Firmware version: %s", result)
+        return result
+
+    @staticmethod
+    def read_hw_revision(serial: Serial) -> Version:
+        result = LiteVNA64._read_major_minor_version(
+            _ADDR_DEVICE_VARIANT, _ADDR_HARDWARE_REVISION, serial
+        )
+        logger.debug(
+            "Hardware version ({device_variant}.{hardware_revision}): %s",
+            result,
+        )
+        return result
+
+    @staticmethod
+    def is_lite_vna_64(serial: Serial) -> bool:
+        hw_version = LiteVNA64.read_hw_revision(serial)
+        fw_version = LiteVNA64.read_fw_version(serial)
+        return (
+            hw_version == EXPECTED_HW_VERSION
+            and fw_version == EXPECTED_FW_VERSION
+        )
