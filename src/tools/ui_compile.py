a681a9126b4e987b9d8de90c97159c600ec861dd
Maxim Medvedev <redrathnure@gmail.com>
2025-01-22 22:52:45 +0100

    feat: add UI resources

--- src/tools/ui_compile.py
+++ src/tools/ui_compile.py
@@ -5,11 +5,11 @@ from pathlib import Path
 
 def main() -> None:
     print("Generation python classes for ui files...")
-    for p in glob.glob('./src/**/*.ui', recursive=True):
+    for p in glob.glob("./src/**/*.ui", recursive=True):
         handle_ui(Path(p))
 
     print("Generation resource files...")
-    for p in glob.glob('./src/**/*.qrc', recursive=True):
+    for p in glob.glob("./src/**/*.qrc", recursive=True):
         handle_qrc(Path(p))
 
 
@@ -18,18 +18,23 @@ def handle_ui(ui_file: Path) -> None:
     ui_file_name = ui_file.name
     python_name = f'{ui_file_name.replace(".ui", "")}.py'
 
-
-    cmd = ["pyside6-uic", str(ui_file), "-o", str(dir/python_name)]
+    cmd = [
+        "pyside6-uic",
+        str(ui_file),
+        "-o",
+        str(dir / python_name),
+        "--from-imports",
+    ]
     print(f'{" ".join(cmd)}')
     subprocess.run(cmd, check=True)
+
 
 def handle_qrc(qrc_file: Path) -> None:
     dir = qrc_file.parent
     ui_file_name = qrc_file.name
     python_name = f'{ui_file_name.replace(".qrc", "")}_rc.py'
 
-
-    cmd = ["pyside6-rcc", str(qrc_file), "-o", str(dir/python_name)]
+    cmd = ["pyside6-rcc", str(qrc_file), "-o", str(dir / python_name)]
     print(f'{" ".join(cmd)}')
     subprocess.run(cmd, check=True)
 
