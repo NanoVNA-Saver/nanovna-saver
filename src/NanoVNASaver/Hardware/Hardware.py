7318382cbf8776612b740acdb345bb94fa136f57
Holger Müller <zarath@gmx.de>
2025-02-28 20:33:01 +0100

    Update src/NanoVNASaver/Hardware/Hardware.py

--- src/NanoVNASaver/Hardware/Hardware.py
+++ src/NanoVNASaver/Hardware/Hardware.py
@@ -143,7 +143,6 @@ def get_portinfos() -> list[str]:
 def get_VNA(iface: Interface) -> VNA:
     # serial_port.timeout = TIMEOUT
     return NAME2DEVICE[iface.comment](iface)
-
 def get_comment(iface: Interface) -> str:
     logger.info("Finding correct VNA type...")
     with iface.lock:
